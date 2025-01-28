d69c52dd7825c9f228b87e928099a50c3f19a425
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2025-01-28 16:30:08 +0530

    changes

--- src/Library/Main.java
+++ src/Library/Main.java
@@ -1,6 +1,16 @@
 package Library;
 
+import java.util.Scanner;
+
 
 public class Main {
-protected String name;
+public static void main(String[] args){
+    System.out.println("Welcome to library management system!\n" + "1.Login\n2. new user");
+    Scanner s= new Scanner(System.in);
+    int n= s.nextInt();
+
+    switch (n){
+        
+    }
+}
 }
