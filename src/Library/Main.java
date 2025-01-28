084b01a7937850a73f53f2ae82b069a44f7a31ce
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2025-01-28 16:45:33 +0530

    changes

--- src/Library/Main.java
+++ src/Library/Main.java
@@ -4,20 +4,36 @@ import java.util.Scanner;
 
 
 public class Main {
+
+    static Scanner s;
 public static void main(String[] args){
     System.out.println("Welcome to library management system!\n" + "1.Login\n2. new user");
-    Scanner s= new Scanner(System.in);
+     s= new Scanner(System.in);
     int n= s.nextInt();
 
     switch (n){
         case 1: login();
         case 2: newuser();
-        default: System.err.println("Error");
+        default: System.out.println("Error");
     }
 }
 
-private void login(){
-    
+private static void login(){
+    System.out.println("Enter phone number");
+    String phonenumber = s.next();
+    System.out.println("Enter email");
+    String email = s.next();
 }
+
+private static void newuser(){
+    System.out.println("Enter name:");
+    String name = s.next();
+    System.out.println("Enter phone number");
+    String phonenumber = s.next();
+    System.out.println("Enter email");
+    String email = s.next();
+}
+
+
 
 }
