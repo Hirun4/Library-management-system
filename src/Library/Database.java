ef68c8bd4d43e13e69ef606e9c2f7a0a22df95d5
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2025-01-31 09:20:03 +0530

    database

--- src/Library/Database.java
+++ src/Library/Database.java
@@ -1,5 +1,25 @@
 package Library;
 
+import java.util.ArrayList;
+
 public class Database {
 
+    ArrayList<User> users= new ArrayList<User>();
+    ArrayList<String> usernames= new ArrayList<String>();
+    public void AddUser(User s){
+        users.add(s);
+        usernames.add(s.getName());
+    }
+
+    public boolean login(String phonenumber,String email) {
+        boolean n = false;
+        for(User s : users){
+            if (s.getPhoneNumber().matches(phonenumber) && s.getEmail().matches(email)) {
+                n= true;
+                break;
+                
+            }
+        }
+        return n;
+    }
 }
--- src/Library/Main.java
+++ src/Library/Main.java
@@ -2,42 +2,47 @@ package Library;
 
 import java.util.Scanner;
 
-
 public class Main {
 
     static Scanner s;
-public static void main(String[] args){
-    System.out.println("Welcome to library management system!\n" + "1.Login\n2. new user");
-     s= new Scanner(System.in);
-    int n= s.nextInt();
 
-    switch (n){
-        case 1: login();
-        case 2: newuser();
-        default: System.out.println("Error");
+    public static void main(String[] args) {
+
+        Database database= new Database();
+        System.out.println("Welcome to library management system!\n" + "1.Login\n2. new user");
+        s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        switch (n) {
+            case 1:
+                login();
+            case 2:
+                newuser();
+            default:
+                System.out.println("Error");
+        }
     }
-}
 
-private static void login(){
-    System.out.println("Enter phone number");
-    String phonenumber = s.next();
-    System.out.println("Enter email");
-    String email = s.next();
-}
+    private static void login() {
+        System.out.println("Enter phone number");
+        String phonenumber = s.next();
+        System.out.println("Enter email");
+        String email = s.next();
+    }
 
-private static void newuser(){
-    System.out.println("Enter name:");
-    String name = s.next();
-    System.out.println("Enter phone number");
-    String phonenumber = s.next();
-    System.out.println("Enter email");
-    String email = s.next();
-    int n2= s.nextInt();
-    if (n2 ==1) {
-        
-    }else
-}
-
-
+    private static void newuser() {
+        System.out.println("Enter name:");
+        String name = s.next();
+        System.out.println("Enter phone number");
+        String phonenumber = s.next();
+        System.out.println("Enter email");
+        String email = s.next();
+        int n2 = s.nextInt();
+        if (n2 == 1) {
+            User admin = new Admn(name, email, phonenumber);
+        } else {
+            User user = new User(name, email, phonenumber);
+        }
+    }
 
 }
